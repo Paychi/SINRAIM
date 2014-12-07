12a4db4b5a44a0d2f03a284bd5be48e2892cafee
Alvin Baltodano <alvinbal93@gmail.com>
2014-12-06 23:57:42 -0600

    Modificaciones Galeria
    Contedido de reacciones adversas

--- app/views/site/visorimage.blade.php
+++ app/views/site/visorimage.blade.php
@@ -11,7 +11,7 @@ Galeria
 @section('content')
 		
 	<h2 id="txtM">
-            Reacciones Adversas Causadas por Insumos Medicos
+            Galer&iacute;a de im&aacute;genes de Reacciones Adversas Causadas por Insumos Medicos
     </h2>
     
     <section class="section" id="sec">
@@ -31,7 +31,13 @@ Galeria
 				{{HTML::image('img/reaccion_adversa/sind-lyell_3.png','sind-lyell_3')}}
 			</figure>
 		</article>
-		<p class="colortexto_galeria">Descripcion de la resaccion adversa al insumo medico suministado<p>				
+		<p class="colortexto_galeria">
+			La necrólisis epidérmica tóxica es una enfermedad cutánea grave caracterizada por la aparición de ampollas y lesiones 
+			exfoliativas de la piel, por lo general provocado por la reacción a un medicamento, frecuentemente antibióticos o 
+			anticonvulsivantes. La necrólisis epidérmica tóxica comparte aspectos causales, patogenéticos, histológicos y terapéuticos
+			con el síndrome de Stevens-Johnson y considerada una enfermedad distinta del eritema multiforme. Tiene una 
+			mortalidad de aproximadamente el 30%
+		<p>				
 	</section>
 	
 	 <section class="section" id="sec">
@@ -56,7 +62,13 @@ Galeria
 				{{HTML::image('img/reaccion_adversa/sind-stevensjohnson_4.png','sind-stevensjohnson_4')}}
 			</figure>
 		</article>
-		<p class="colortexto_galeria">Descripcion de la resaccion adversa al insumo medico suministado<p>				
+		<p class="colortexto_galeria">
+			El síndrome de Stevens-Johnson es el eritema multiforme mayor, o sea, la más grave de sus manifestaciones.
+			La forma más grave es causada generalmente por reacciones a medicamentos, en lugar de infecciones. 
+			Es una variante severa del eritema multiforme, consistente en una reacción de hipersensibilización que afecta la 
+			piel y las membranas mucosas. Adicionalmente, pueden quedar implicados algunos otros órganos. El síndrome de 
+			Stevens-Johnson y la necrólisis epidérmica tóxica tienen tasas altas de mortalidad.
+		<p>				
 	</section>
 	
 @stop
